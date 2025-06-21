7ca843c8978bb0aa5b27d3852c773dbc4e8400a4
Jacobo Santos <jsantoscores@gmail.com>
2025-06-21 10:32:42 +0200

    set up module structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { CoreModule } from './core/core-module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    importProvidersFrom(CoreModule),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: 'library',
+    loadChildren: () =>
+      import('./library/library-module').then((m) => m.LibraryModule)
+  },
+  { path: '', redirectTo: 'library', pathMatch: 'full' }];
